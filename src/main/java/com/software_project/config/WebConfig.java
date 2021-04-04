d42f794ce44a6c6cf5126da8492c2832749536dc
yangxi0829 <1263996368@qq.com>
2021-04-04 16:20:36 +0800

    支持跨域请求

--- src/main/java/com/software_project/config/WebConfig.java
+++ src/main/java/com/software_project/config/WebConfig.java
@@ -1,5 +1,6 @@
-package com.software_project.interceptor;
+package com.software_project.config;
 
+import com.software_project.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
