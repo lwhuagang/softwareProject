163dca47c25fa3fc2cdf29ee9c90c4ad8773bc34
panyi <2428541469@qq.com>
2021-04-28 18:34:38 +0800

    添加功能:当用户未登录的时候，点击自选和持有会弹出未登录的提示，点击确定跳转到我的页面，点击取消跳转到首页

--- frontend/pages/selfselect/selfselect.js
+++ frontend/pages/selfselect/selfselect.js
@@ -10,24 +10,47 @@ Page({
    * 页面的初始数据
    */
   data: {
-    "funds": []
+    "funds": [],
+    isLogin:false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log("获得selfSelect===>");
-    getSelfSelect(
-      app.globalData.userInfo.email,
-      res => {
-        this.setData({
-          funds: res.data.obj.funds
-        });
-        console.log("获得selfSelect===>");
-        console.log(res);
-      }
-    )
+    this.setData({
+      isLogin:app.globalData.isLogin
+    })
+    if(app.globalData.isLogin==false) {
+      wx.showModal({
+        cancelColor: 'cancelColor',
+        title:'请先登录',
+        content:'点击确定跳转到登录界面',
+        success(res) {
+          if(res.confirm) {
+           wx.switchTab({
+             url: '/pages/mine/mine',
+           })
+          } else if(res.cancel) {
+            wx.switchTab({
+              url: '/pages/index/index',
+            })
+          }
+        }
+      })
+    } else {
+      console.log("获得selfSelect===>");
+      getSelfSelect(
+        app.globalData.userInfo.email,
+        res => {
+          this.setData({
+            funds: res.data.obj.funds
+          });
+          console.log("获得selfSelect===>");
+          console.log(res);
+        }
+      )
+    }
   },
 
   /**
@@ -41,16 +64,39 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    getSelfSelect(
-      app.globalData.userInfo.email,
-      res => {
-        this.setData({
-          funds: res.data.obj.funds
-        });
-        console.log("获得selfSelect===>");
-        console.log(res);
-      }
-    )
+    this.setData({
+      isLogin:app.globalData.isLogin
+    })
+    if(app.globalData.isLogin==false) {
+      wx.showModal({
+        cancelColor: 'cancelColor',
+        title:'请先登录',
+        content:'点击确定跳转到登录界面',
+        success(res) {
+          if(res.confirm) {
+           wx.switchTab({
+             url: '/pages/mine/mine',
+           })
+          } else if(res.cancel) {
+            wx.switchTab({
+              url: '/pages/index/index',
+            })
+          }
+        }
+      })
+    } else {
+      getSelfSelect(
+        app.globalData.userInfo.email,
+        res => {
+          this.setData({
+            funds: res.data.obj.funds
+          });
+          console.log("获得selfSelect===>");
+          console.log(res);
+        }
+      )
+    }
+
   },
 
   /**
