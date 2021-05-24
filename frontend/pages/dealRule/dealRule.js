5e1c8b2d872b83dbfbb96d3ce4ce047cec530331
陶卓 <18375238@buaa.edu.cn>
2021-05-24 16:15:30 +0800

    卖出规则界面已完成

--- frontend/pages/dealRule/dealRule.js
+++ frontend/pages/dealRule/dealRule.js
@@ -1,4 +1,12 @@
 // pages/dealRule/dealRule.js
+let {
+  getFund,
+  getFundPosition,
+  getFundRank,
+  getFundDetail,
+  getAllFund,
+  getHotFund,
+} = require("../../api/getFoundation.js");
 Page({
 
   /**
@@ -8,17 +16,33 @@ Page({
     fundInfo:null,
     selectRule:"买入规则",
     buyInStart:"15:00",
-    buyInRateList:[
-      {step:"0≤买入金额<100万",srcRate:"1.50%",rate:"0.15%"},
-      {step:"100万≤买入金额<300万",srcRate:"0.90%",rate:"0.09%"},{step:"300万≤买入金额<500万",srcRate:"0.30%",rate:"0.03%"},{step:"500万≤买入金额<1000万",srcRate:"0.10%",rate:"0.01%"},{step:"1000万≤买入金额",srcRate:"1000元",rate:"1000元"}
-    ]
+    // buyInRateList:[
+    //   {step:"0≤买入金额<100万",srcRate:"1.50%",rate:"0.15%"},
+    //   {step:"100万≤买入金额<300万",srcRate:"0.90%",rate:"0.09%"},{step:"300万≤买入金额<500万",srcRate:"0.30%",rate:"0.03%"},{step:"500万≤买入金额<1000万",srcRate:"0.10%",rate:"0.01%"},{step:"1000万≤买入金额",srcRate:"1000元",rate:"1000元"}
+    // ],
+    buyInRateList:[{step:"0≤买入金额<+∞",srcRate:"1.50%",rate:"0.15%"},],
+    sellOutRateList:[{step:"0≤卖出金额<+∞",srcRate:"1%",rate:"0.1%"},],
+    fundCode:""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      fundCode:options.fundCode
+    })
+    console.log(this.data.fundCode)
+    var data = {
+      code:this.data.fundCode
+    }
+    getFundDetail(data,(res)=>{
+      console.log(res)
+      var buyInRateList = [{step:"0≤买入金额<+∞",srcRate:res.data.data.buySourceRate+"%",rate:res.data.data.buyRate+"%"},]
+      this.setData({
+        buyInRateList:buyInRateList
+      })
+    })
   },
   swap2BuyIn:function() {
     this.setData({
